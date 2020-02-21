f489268d44801d3e46ea4bbe439f258eb7039793
Julien Bras <julienbras@sidoine.org>
2020-02-20 20:25:05 -0500

    remove routes without v1

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var v1 = require('./v1')
-var nomenclatures = require('./nomenclatures')
 
 // the root is redirecting to the Swagger documentation
 router.get('/', function(req, res, next) {
@@ -11,8 +10,5 @@ router.get('/', function(req, res, next) {
 
 // redirect v1 to the v1 router
 router.use('/v1', v1)
-
-// redirect nomenclature to the nomenclature router (to remove)
-router.use('/nomenclatures', nomenclatures)
 
 module.exports = router
