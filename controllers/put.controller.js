4c00985bf3a8253a462e45a55708e50ae0dba94e
Julien Bras <julienbras@sidoine.org>
2020-02-22 09:56:52 -0500

    fix an issue on PUT endpoint that was not updating

--- controllers/put.controller.js
+++ controllers/put.controller.js
@@ -4,7 +4,15 @@ const Nomenclature = require('../models/nomenclature.model');
 module.exports = async(req, res, next) => {
   try {
     let nomenclature = await Nomenclature.findOne({_id: req.params.id})
-    doc = req.body
+    
+    //update fields
+    for (var field in Nomenclature.schema.paths) {
+       if ((field !== '_id') && (field !== '__v')) {
+          if (req.body[field] !== undefined) {
+             nomenclature[field] = req.body[field];
+          }
+       }
+    }
     await nomenclature.save()
     res.json({msg: "Object " + req.params.id + " updated."})
   }
