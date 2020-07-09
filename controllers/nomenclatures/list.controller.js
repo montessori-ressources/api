038f70687f97547c49c71d02d42cac3936776eff
Julien Bras <julienbras@sidoine.org>
2020-07-08 22:10:28 -0400

    fix #15 to allow usage of sort in nomenclature

--- controllers/nomenclatures/list.controller.js
+++ controllers/nomenclatures/list.controller.js
@@ -3,7 +3,14 @@ const Nomenclature = require('../../models/nomenclature.model');
 
 module.exports = async(req, res, next) => {
   try {
-    let docs = await Nomenclature.find()
+
+    let docsPromise = Nomenclature.find()
+    if(req.query.sort !== undefined) {
+      let sort = JSON.parse(req.query.sort);
+      docsPromise.sort([sort])
+    }
+    let docs = await docsPromise
+    
     if(req.query.range !== undefined) {
       let range = JSON.parse(req.query.range);
       res.set('Content-Range', `items ${range[0]}-${range[1]}/${docs.length}`);
